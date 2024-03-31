9d47af1d7ad720bfd98364b50d16fbd17ad51505
gunhee <120537859+gunhee0421@users.noreply.github.com>
2024-03-31 19:53:52 +0900

    [Silver I] Title: 그림, Time: 460 ms, Memory: 57348 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,63 +1,70 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int count = 0;
-    static int[][] arr;
-    static boolean[][] visite;
-
+    static int[][] grah;
+    static boolean[][] visited;
+    static PriorityQueue<Integer> result = new PriorityQueue<>(Collections.reverseOrder());
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n][m];
-        visite=new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(visite[i], false);
-        }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        grah = new int[n][m];
+        visited = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                grah[i][j] = Integer.parseInt(st.nextToken());
             }
+            Arrays.fill(visited[i], false);
         }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                count = 0;
-                if (!visite[i][j] && arr[i][j] == 1) {
-                    dfs(i, j);
-                    pq.add(count);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && grah[i][j]==1) {
+                    bfs(i, j);
+                    result.add(count);
                 }
             }
         }
-        System.out.println(pq.size());
-        if (pq.isEmpty()) {
+        System.out.println(result.size());
+        if (result.isEmpty()) {
             System.out.println(0);
         } else {
-            System.out.println(pq.remove());
+            System.out.println(result.poll());
         }
     }
-    public static void dfs(int x, int y) {
-        visite[x][y] = true;
-        count++;
 
-        int[][] pos = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        for (int i = 0; i < pos.length; i++) {
-            int nx = x + pos[i][0];
-            int ny = y + pos[i][1];
+    public static void bfs(int x, int y) {
+        count=1;
+        int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        visited[x][y] = true;
 
-            if (nx > -1 && nx < arr.length && ny > -1 && ny < arr[0].length && !visite[nx][ny] && arr[nx][ny] == 1) {
-                dfs(nx, ny);
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int nowx = now[0];
+            int nowy = now[1];
+
+            for (int i = 0; i < pos.length; i++) {
+                x = nowx + pos[i][0];
+                y = nowy + pos[i][1];
+                if (x >= 0 && x < grah.length && y >= 0 && y < grah[0].length && grah[x][y] == 1 && !visited[x][y]) {
+                    count++;
+                    q.offer(new int[]{x, y});
+                    visited[x][y] = true;
+                }
             }
+
         }
+
     }
 }
