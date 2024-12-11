0d5be9e0dbf6afc93ee30b91beb710eac7f731d1
jogunhee <rjsgml771@kangnam.ac.kr>
2024-12-11 21:36:52 +0900

    [Silver I] Title: 카잉 달력, Time: 208 ms, Memory: 11032 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.js
@@ -0,0 +1,33 @@
+const [T, ...input] = require("fs")
+  .readFileSync("/dev/stdin")
+  // .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+const data = input.map((v) => v.split(" ").map(Number));
+
+const getGCD = (a, b) => {
+  let x = Math.max(a, b);
+  let y = Math.min(a, b);
+  let remainder;
+  while (y) {
+    remainder = x % y;
+    x = y;
+    y = remainder;
+  }
+  return x;
+};
+
+const output = data.map(([M, N, x, y]) => {
+  const lcm = (M * N) / getGCD(M, N);
+  let year;
+  for (let i = x; i <= lcm; i += M) {
+    if (((i - 1) % N) + 1 === y) {
+      year = i;
+      break;
+    }
+  }
+  return year ? year : -1;
+});
+
+console.log(output.join("\n"));
