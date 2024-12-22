b528b4ea7856ccd9bd5464f52ad3afd378789a79
jogunhee <rjsgml771@kangnam.ac.kr>
2024-12-22 14:23:33 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,28 @@
+const [N, M] = require("fs")
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const Combination = (N, M) => {
+  const res = [];
+  const combination = [];
+
+  const backtracking = (start) => {
+    if (combination.length === M) {
+      res.push([...combination]);
+      return;
+    }
+    for (let i = start; i <= N; i++) {
+      combination.push(i);
+      backtracking(i + 1);
+      combination.pop();
+    }
+  };
+  backtracking(1);
+  return res;
+};
+
+const result = Combination(N, M);
+console.log(result.map((t) => t.join(" ")).join("\n"));
