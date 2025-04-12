0e4a8fe9088517e4a45693a5d3f2a9d58cf80464
jogunhee <rjsgml771@naver.com>
2025-04-12 16:29:50 +0900

    [Gold IV] Title: 알파벳, Time: 3176 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -6,13 +6,13 @@ const input = require("fs")
   .split("\n");
 
 /*
-1. set으로 중복 검사 후 패스
+1. set으로 중복 검사 후 패스 -> 26개의 알파벳 배열로 변경
 2. dfs로 깊이탐색
 */
 function solution(input) {
   const [R, C] = input[0].split(" ").map(Number);
   const board = input.slice(1).map((row) => row.split(""));
-  const visited = new Set();
+  const visited = Array(26).fill(false);
   const directions = [
     [-1, 0],
     [1, 0],
@@ -26,24 +26,22 @@ function solution(input) {
   return res;
 
   function dfs(x, y, count) {
-    visited.add(board[x][y]);
+    const cur = board[x][y].charCodeAt() - 65;
+    visited[cur] = true;
     res = Math.max(res, count);
 
     for (const [dx, dy] of directions) {
       const nx = x + dx;
       const ny = y + dy;
 
-      if (
-        nx >= 0 &&
-        nx < R &&
-        ny >= 0 &&
-        ny < C &&
-        !visited.has(board[nx][ny])
-      ) {
-        dfs(nx, ny, count + 1);
+      if (nx >= 0 && nx < R && ny >= 0 && ny < C) {
+        const next = board[nx][ny].charCodeAt() - 65;
+        if (!visited[next]) {
+          dfs(nx, ny, count + 1);
+        }
       }
     }
-    visited.delete(board[x][y]);
+    visited[cur] = false;
   }
 }
 
