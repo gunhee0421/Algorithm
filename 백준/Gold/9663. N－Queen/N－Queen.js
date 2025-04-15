dca0db7d787325ca8b66f2bb84be044870a93e74
jogunhee <rjsgml771@naver.com>
2025-04-15 09:54:31 +0900

    [Gold IV] Title: N-Queen, Time: 7020 ms, Memory: 10196 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.js
+++ 백준/Gold/9663. N－Queen/N－Queen.js
@@ -8,35 +8,36 @@ const input = require("fs")
 /*
 1. 퀸은 가로, 세로, 대각선으로 이동할 수 있다. -> 가로, 세로, 대각선에 있는 퀸을 체크해야함.
 2. row별 놓을 수 있는 퀸의 개수를 카운트
+3. 2차원배열이 아닌, 1차원 row배열에 col을 넣음으로써 시간 단축 가능
 */
 function solution(input) {
   const N = Number(input[0]);
-  const board = Array.from({ length: N }, () => Array(N).fill(0));
+  const board = Array(N).fill(0);
 
   let res = 0;
 
-  dfs(0);
+  backtrack(0);
 
   return res;
 
-  function dfs(row) {
+  function backtrack(row) {
     if (row === N) {
       res++;
       return;
     }
     for (let col = 0; col < N; col++) {
       if (isValid(row, col)) {
-        board[row][col] = 1;
-        dfs(row + 1);
-        board[row][col] = 0;
+        board[row] = col;
+        backtrack(row + 1);
+        board[row] = 0;
       }
     }
   }
   function isValid(row, col) {
     for (let i = 0; i < row; i++) {
-      if (board[i][col] === 1) return false;
-      if (col - (row - i) >= 0 && board[i][col - (row - i)] === 1) return false;
-      if (col + (row - i) < N && board[i][col + (row - i)] === 1) return false;
+      if (board[i] === col || Math.abs(row - i) === Math.abs(col - board[i])) {
+        return false;
+      }
     }
     return true;
   }
